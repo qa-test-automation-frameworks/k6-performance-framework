9c80b7a283fe9d48b17c4e16a22aa981b0cd4f2c
prayagv <prayagv@users.noreply.github.com>
2026-06-12 14:46:32 +0530

    feat: add browse and authenticated CRUD scenarios

---
+++ src/scenarios/authenticated-crud.ts
@@ -0,0 +1,26 @@
+import { group, sleep } from 'k6';
+import { createApi } from '../api';
+import { articleFixture } from '../fixtures';
+import { assertWriteTarget, checkStatus, requireData, tokenForVu } from '../helpers';
+
+export function authenticatedCrud(): void {
+  assertWriteTarget();
+  const token = tokenForVu();
+  const api = createApi();
+  group('authenticated article CRUD', () => {
+    const created = api.articles.create(token, articleFixture());
+    checkStatus(created, 201, 'create article');
+    const slug = requireData(created, 'create article').article.slug;
+    try {
+      checkStatus(api.articles.update(token, slug, { description: 'Updated by k6' }), 200,
+        'update article');
+      checkStatus(api.comments.create(token, slug, { body: 'Performance comment' }), 200,
+        'create comment');
+      checkStatus(api.articles.favorite(token, slug), 200, 'favorite article');
+      checkStatus(api.articles.unfavorite(token, slug), 200, 'unfavorite article');
+    } finally {
+      checkStatus(api.articles.delete(token, slug), 204, 'delete article');
+    }
+  });
+  sleep(1);
+}
