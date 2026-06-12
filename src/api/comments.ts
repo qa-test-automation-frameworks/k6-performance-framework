4cb23d070304810757970187c23e047a4323c6df
prayagv <prayagv@users.noreply.github.com>
2026-06-12 14:46:06 +0530

    feat: add typed Conduit API services

---
+++ src/api/comments.ts
@@ -0,0 +1,22 @@
+import type { CommentPayload, CommentResponse, CommentsResponse, EmptyResponse, HttpResponse } from '../types';
+import { HttpClient } from '../utils/http-client';
+import { auth } from './auth';
+
+export class CommentsService {
+  constructor(private readonly client = new HttpClient()) {}
+
+  list(slug: string): HttpResponse<CommentsResponse> {
+    return this.client.get(`/articles/${encodeURIComponent(slug)}/comments`,
+      'GET /articles/:slug/comments');
+  }
+
+  create(token: string, slug: string, payload: CommentPayload): HttpResponse<CommentResponse> {
+    return this.client.post(`/articles/${encodeURIComponent(slug)}/comments`,
+      'POST /articles/:slug/comments', { comment: payload }, { params: auth(token) });
+  }
+
+  delete(token: string, slug: string, id: number): HttpResponse<EmptyResponse> {
+    return this.client.delete(`/articles/${encodeURIComponent(slug)}/comments/${id}`,
+      'DELETE /articles/:slug/comments/:id', { params: auth(token) });
+  }
+}
